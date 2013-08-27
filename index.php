f4f7e439f682670b6ceb26dd7f0c1599dab8cb31
Mark Constable <markc@renta.net>
2013-08-27 15:36:32 +1000

    Make sure the navbar collapses with mobile view

--- index.php
+++ index.php
@@ -53,20 +53,20 @@ footer { text-align: center; color: #7F7F7F; font-style: italic; font-family: se
     </style>
   </head>
   <body>
-    <div class="navbar navbar-fixed-top navbar-inverse">
+    <div class="navbar navbar-inverse navbar-fixed-top">
       <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".nav-collapse">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="/"><?= TITLE ?></a>
         </div>
-        <div class="navbar-collapse collapse">
+        <div class="collapse navbar-collapse">
           <ul class="nav navbar-nav"><?= $menu ?>
           </ul>
-        </div>
+        </div><!--/.nav-collapse -->
       </div>
     </div>
     <div class="container"><?= $html ?>
