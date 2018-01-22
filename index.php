ea1b44e81c22dd7d84b97a4acb01e5ead2b619a5
Mark Constable <markc@renta.net>
2018-01-23 01:29:40 +1100

    Replace css with lightweight custom style.css

--- index.php
+++ index.php
@@ -1,91 +1,86 @@
 <?php
-// bootstrap_markdown 20130811 (C) Mark Constable (AGPL-3.0)
-// A simple script to render Markdown files within a Bootstrap3 contianer.
+// index.php 20130811 - 20171031
+// Copyright (C) 1995-2017 Mark Constable <markc@renta.net> (AGPL-3.0)
+// A simple script to render Markdown files within a single page.
 
-define('TITLE', 'alsa.opensrc.org');
-define('FOOTER', '&copy; 2013 OpenSrc Team (AGPL-3.0)');
+define('DESC', 'Independent ALSA and linux audio support site');
+define('TITLE', 'Alsa Opensrc Org');
+define('FOOTER', 'Copyright &copy; 2013-2018 OpenSrc Team (AGPL-3.0)');
 define('BLOB', 'https://github.com/opensrc/alsa/blob/master/');
 define('EDIT', 'https://github.com/opensrc/alsa/edit/master/');
 
 require 'lib/php/Markdown.php';
 use \Michelf\Markdown;
 
-$page = isset($_GET['q']) ? htmlspecialchars(trim($_GET['q'], '/')) : '';
+//error_log(var_export($_SERVER, true));
+//$page = isset($_GET['q']) ? htmlspecialchars(trim($_GET['q'], '/')) : '';
+$page = isset($_SERVER['REQUEST_URI']) ? htmlspecialchars(trim($_SERVER['REQUEST_URI'], '/')) : '';
+error_log("page = $page");
 
 if ($page) {
     if (file_exists('lib/md/' . $page . '.md')) {
         $html = Markdown::defaultTransform(file_get_contents('lib/md/' . $page . '.md')).'
-        <div class="edit">
-          <a href="'.BLOB.'lib/md/'.$page.'.md">GITHUB</a> |
-          <a href="'.EDIT.'lib/md/'.$page.'.md">EDIT</a>
-        </div>';
+<div class="right">
+<small>
+<a href="'.BLOB.'lib/md/'.$page.'.md">GITHUB</a> |
+<a href="'.EDIT.'lib/md/'.$page.'.md">EDIT</a>
+</small>
+</div>';
     } else {
         header('HTTP/1.0 404 Not Found');
         $html = '<h1 class="text-center">404 Page Not Found</h1>';
     }
+    $class = '';
 } else {
     $html = include 'lib/php/navigation.php';
+    $class = ' intro';
 }
 
 $menu = '';
 $menu_ary = [
-    ['README', '<i class="fa fa-file-text"></i>'],
-    ['Github', '<i class="fa fa-github-alt"></i>'],
-    ['User:Markc', '<i class="fa fa-user"></i>'],
+    ['README', '<span class=icon-newspaper-o></span>'],
+    ['Github', '<span class=icon-globe></span>'],
+    ['User:Markc', '<span class=icon-user></span>'],
 ];
 
 foreach($menu_ary as $m) {
-    $c = $m[0] == $page ? ' class="active"' : '';
+    $c = $m[0] == $page ? ' active' : '';
     $menu .= '
-            <li'.$c.'><a href="/'.$m[0].'">'.$m[1].' '.$m[0].'</a></li>';
+<a class="link'.$c.'" href="/'.$m[0].'">'.$m[1].' <b>'.$m[0].'</b></a>';
 }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="keywords" content="alsa linux audio">
-    <meta name="description" content="ALSA support site">
-    <meta name="author" content="(C) 2013-2014 Mark Constable (AGPL-3.0)">
-    <meta name="google-site-verification" content="L4CmEiRi_2aoQ3T2CYsZSQpBa05c3wZuIBONMmrgugU">
-    <title><?= TITLE ?></title>
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
-    <style>
-body { padding-top: 50px; overflow-y: scroll; }
-h1, h2, h3, h4 { border-bottom: 1px solid #eee; padding-bottom: 2px; margin-top: 20px; }
-footer { text-align: center; color: #7F7F7F; font-style: italic; font-family: serif; }
-.edit { font-size: 50%; float: right; }
-@media (min-width: 768px) { .container { max-width: 760px; } }
-    </style>
-  </head>
-  <body>
-    <div class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/"><i class="fa fa-home"></i> <?= TITLE ?></a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav"><?= $menu ?>
-
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </div>
-    <div class="container">
-      <?= $html ?>
-
-    </div>
-    <footer><?= FOOTER ?>
-    </footer>
-    <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-  </body>
+<!doctype html>
+<html lang=en>
+<head>
+<meta charset=UTF-8>
+<meta name=viewport content="width=device-width, initial-scale=1">
+<meta name=description content="<?=TITLE?> - <?=DESC?>">
+<title><?=TITLE?> - <?=DESC?></title>
+<link rel="shortcut icon" href="/lib/img/favicon.ico" type="image/x-icon">
+<link href="/lib/css/style.css" rel=stylesheet>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-60445335-5"></script>
+<script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','UA-60445335-5');</script>
+</head>
+<body>
+<header class=navbar>
+<input type=checkbox id=menu-toggle>
+<label for=menu-toggle class=label-toggle></label>
+<nav id=topnav class=container>
+<a class=logo href="/"><?=TITLE?></a><?=$menu?>
+</nav>
+</header>
+<div class=center><i><?=DESC?></i></div>
+<main>
+<div class="container<?=$class?>"><?=$html?>
+</div>
+</main>
+<footer>
+<p class=center>
+<?=FOOTER?><br>
+Hosting provided by
+<a href="https://renta.net" title="RentaNet - Domain, Mail and Web Hosting with extra Storage and Support options"><b>RentaNet</b></a><br>
+</p>
+</footer>
+</body>
 </html>
